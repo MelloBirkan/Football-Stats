c4cd9e3ac6972766bc734ba0735ea1a539669d64
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-03-01 01:59:03 -0300

    Initial Commit

---
+++ Football Stats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football Stats
+//
+//  Created by Marcello Gonzatto Birkan on 01/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football Stats/Football_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_StatsApp.swift
+//  Football Stats
+//
+//  Created by Marcello Gonzatto Birkan on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Football_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
