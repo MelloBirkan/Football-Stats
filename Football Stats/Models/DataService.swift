9000a8cd0f451d635e8a3fb3b6cfb581ee31ac2e
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-03-01 02:26:14 -0300

    Add initial app structure with models, views, and data service

---
+++ Football Stats/Models/DataService.swift
@@ -0,0 +1,36 @@
+//
+//  DataService.swift
+//  Football Stats
+//
+//  Created by Marcello Gonzatto Birkan on 01/03/25.
+//
+
+import Foundation
+
+struct DataService {
+  let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+  
+  func fetchData() async -> Team {
+    guard let apiKey else {
+      print("API key not found.")
+      return Team()
+    }
+    
+    if let url = URL(string: "https://api.football-data.org/v4/teams/10") {
+      var request = URLRequest(url: url)
+      request.addValue(apiKey, forHTTPHeaderField: "X-Auth-Token")
+      
+      do {
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        let result = try decoder.decode(Team.self, from: data)
+        return result
+        
+      } catch {
+        print(error)
+      }
+    }
+    return Team()
+  }
+}
---
+++ Football Stats/Models/Team.swift
@@ -0,0 +1,186 @@
+//
+//  Team.swift
+//  Football Stats
+//
+//  Created by Marcello Gonzatto Birkan on 01/03/25.
+//
+
+import Foundation
+
+struct Area: Codable {
+  let id: Int
+  let name: String
+  let code: String
+  let flag: String?
+
+  init(id: Int, name: String, code: String, flag: String? = nil) {
+    self.id = id
+    self.name = name
+    self.code = code
+    self.flag = flag
+  }
+}
+
+struct Competition: Codable {
+  let id: Int
+  let name: String
+  let code: String
+  let type: String
+  let emblem: String?
+
+  init(id: Int, name: String, code: String, type: String, emblem: String? = nil) {
+    self.id = id
+    self.name = name
+    self.code = code
+    self.type = type
+    self.emblem = emblem
+  }
+}
+
+struct Contract: Codable {
+  let start: String
+  let until: String
+
+  init(start: String, until: String) {
+    self.start = start
+    self.until = until
+  }
+}
+
+struct Coach: Codable {
+  let id: Int
+  let firstName: String
+  let lastName: String
+  let name: String
+  let dateOfBirth: String
+  let nationality: String
+  let contract: Contract
+
+  init(id: Int, firstName: String, lastName: String, name: String, dateOfBirth: String, nationality: String, contract: Contract) {
+    self.id = id
+    self.firstName = firstName
+    self.lastName = lastName
+    self.name = name
+    self.dateOfBirth = dateOfBirth
+    self.nationality = nationality
+    self.contract = contract
+  }
+}
+
+struct Player: Codable {
+  let id: Int
+  let firstName: String?
+  let lastName: String?
+  let name: String
+  let position: String
+  let dateOfBirth: String
+  let nationality: String
+  let shirtNumber: Int?
+  let marketValue: Int?
+  let contract: Contract
+
+  init(id: Int, firstName: String? = nil, lastName: String? = nil, name: String, position: String, dateOfBirth: String, nationality: String, shirtNumber: Int? = nil, marketValue: Int? = nil, contract: Contract) {
+    self.id = id
+    self.firstName = firstName
+    self.lastName = lastName
+    self.name = name
+    self.position = position
+    self.dateOfBirth = dateOfBirth
+    self.nationality = nationality
+    self.shirtNumber = shirtNumber
+    self.marketValue = marketValue
+    self.contract = contract
+  }
+}
+
+struct Staff: Codable {
+  let id: Int
+  let firstName: String?
+  let lastName: String?
+  let name: String
+  let dateOfBirth: String
+  let nationality: String
+  let contract: Contract
+
+  init(id: Int, firstName: String? = nil, lastName: String? = nil, name: String, dateOfBirth: String, nationality: String, contract: Contract) {
+    self.id = id
+    self.firstName = firstName
+    self.lastName = lastName
+    self.name = name
+    self.dateOfBirth = dateOfBirth
+    self.nationality = nationality
+    self.contract = contract
+  }
+}
+
+struct Team: Codable {
+  let area: Area
+  let id: Int
+  let name: String
+  let shortName: String
+  let tla: String
+  let crest: String
+  let address: String
+  let website: String
+  let founded: Int
+  let clubColors: String
+  let venue: String
+  let runningCompetitions: [Competition]
+  let coach: Coach
+  let marketValue: Int
+  let squad: [Player]
+  let staff: [Staff]
+  let lastUpdated: String
+
+  init() {
+    area = Area(id: 2224, name: "Spain", code: "ESP", flag: "https://crests.football-data.org/760.svg")
+    id = 90
+    name = "Real Betis Balompié"
+    shortName = "Real Betis"
+    tla = "BET"
+    crest = "https://crests.football-data.org/90.png"
+    address = "Avenida de Heliópolis, s/n Sevilla 41012"
+    website = "http://www.realbetisbalompie.es"
+    founded = 1907
+    clubColors = "Green / White"
+    venue = "Estadio Benito Villamarín"
+
+    let defaultCompetition = Competition(id: 2014, name: "Primera Division", code: "PD", type: "LEAGUE", emblem: "https://crests.football-data.org/PD.png")
+    runningCompetitions = [defaultCompetition]
+
+    let defaultContract = Contract(start: "2020-08", until: "2023-06")
+    coach = Coach(id: 11630, firstName: "Manuel", lastName: "Pellegrini", name: "Manuel Pellegrini", dateOfBirth: "1953-09-16", nationality: "Chile", contract: defaultContract)
+
+    marketValue = 225_100_000
+
+    let defaultPlayerContract = Contract(start: "2020-07", until: "2023-06")
+    let defaultPlayer = Player(id: 7821, firstName: "", lastName: "Joel", name: "Joel Robles", position: "Goalkeeper", dateOfBirth: "1990-06-17", nationality: "Spain", shirtNumber: 1, marketValue: 2_000_000, contract: defaultPlayerContract)
+    squad = [defaultPlayer]
+
+    let defaultStaffContract = Contract(start: "2020-08", until: "2023-06")
+    let defaultStaff = Staff(id: 63306, firstName: "", lastName: "Fernando", name: "Fernando Fernández", dateOfBirth: "1979-06-02", nationality: "Spain", contract: defaultStaffContract)
+    staff = [defaultStaff]
+
+    lastUpdated = "2022-05-03T08:22:26Z"
+  }
+
+  init(area: Area, id: Int, name: String, shortName: String, tla: String, crest: String, address: String, website: String, founded: Int, clubColors: String, venue: String, runningCompetitions: [Competition], coach: Coach, marketValue: Int, squad: [Player], staff: [Staff], lastUpdated: String) {
+    self.area = area
+    self.id = id
+    self.name = name
+    self.shortName = shortName
+    self.tla = tla
+    self.crest = crest
+    self.address = address
+    self.website = website
+    self.founded = founded
+    self.clubColors = clubColors
+    self.venue = venue
+    self.runningCompetitions = runningCompetitions
+    self.coach = coach
+    self.marketValue = marketValue
+    self.squad = squad
+    self.staff = staff
+    self.lastUpdated = lastUpdated
+  }
+}
